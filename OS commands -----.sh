bdebb88c2b625423ef7023ba8097fea6f5e04931
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-10 16:23:52 +0530

    Add files via upload

---
+++ OS commands -----.sh
@@ -0,0 +1,66 @@
+OS commands ----->>>>>
+1. ls  ---->>>> listing  directory and file
+
+2. pwd --->>>>> Present working directory
+
+3. cd -->>>>>Changing the directory
+
+4. mkdir -->>> we can make directory 
+
+5. touch ---->>> empty file creation  
+     
+example--like:- touch + file name
+ touch Abhi.txt
+
+
+
+6,ctrl+D ---->>>> to get the directory 
+
+7.  rmdir --->>> to remove the directory we use this command 
+
+8.   rm ---->>>>> to delete a particular file in the running directory   
+
+9.  grep command ------> to grep a pattern name (that searches for a specific text string in one or more files.)
+
+10.  ls -l   -------->>>>  this will give name & details of all the current file in the running directory(time , date ,location ,)
+
+11.  link file creation 
+     |
+   examples   |---->>> ln -& file12txt link
+
+      cat link231 
+
+
+
+
+
+shell is command line 
+
+for exmaple ---->>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+types of shell commands
+
+:--
