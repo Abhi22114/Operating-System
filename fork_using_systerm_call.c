3c6e736366b922fc3e75a9245331c57ab52fb6f2
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-03-15 23:45:59 +0530

    Create fork_using_systerm_call.c

---
+++ fork_using_systerm_call.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+pid_t p;
+printf("before fork\n");
+p=fork();
+if(p==0)
+{
+printf("i am child having id %d\n",getpid());
+printf("my parent's id is %d\n",getppid());
+}else{
+printf("my child's id is %d\n",p);
+printf("i am parent having id %d\n",getpid());
+}
+printf("common\n");
+}
